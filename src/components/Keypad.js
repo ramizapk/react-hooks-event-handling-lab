22bf30461301b369c47262946920dc02d3c75798
Ramiz Ali <ramiz.apk7@gmail.com>
2023-04-26 07:27:48 +0300

    complated assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+
+    function handleChange() {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <input type="password" onChange={handleChange} />
     )
 }
 
